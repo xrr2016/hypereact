7538b366f9f287b499d5cd362d671873d0faf7f2
xrr2016 <xiaoranran1993@outlook.com>
2023-10-19 11:22:50 +0800

    feat: update

---
+++ packages/example/App.tsx
@@ -0,0 +1,13 @@
+// @ts-nocheck
+// import Hypereact from 'hypereact';
+import Hypereact from './hypereact';
+
+/** @jsx Hypereact.createElement */
+const App = (
+  <div style="background: salmon">
+    <h1>Hello World</h1>
+    <h2 style="text-align:right">from Hypereact</h2>
+  </div>
+);
+
+export default App;
--- packages/hypereact/src/index.ts
+++ packages/hypereact/src/index.ts
@@ -1,5 +1,47 @@
-export function hellp(text: string) {
-  console.log(`Hello222 ${text}`);
+const TEXT_ELEMENT = 'TEXT_ELEMENT';
+
+export function createElement(type: string, props: any, ...children: any[]) {
+  return {
+    type,
+    ...props,
+    children: children.map((child) =>
+      typeof child === 'object' ? child : createTextElement(child),
+    ),
+  };
 }
 
-export default 'Hello father 4!';
+function createTextElement(text: string | number) {
+  return {
+    type: TEXT_ELEMENT,
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+export function render(
+  element: { type: string; props: { [x: string]: any; children?: any } },
+  container: { appendChild: (arg0: any) => void },
+) {
+  const dom =
+    element.type == TEXT_ELEMENT
+      ? document.createTextNode('')
+      : document.createElement(element.type);
+  const isProperty = (key: string) => key !== 'children';
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      // @ts-ignore
+      dom[name] = element.props[name];
+    });
+  element.props.children.forEach((child: any) => render(child, dom));
+  container.appendChild(dom);
+}
+
+const Hypereact = {
+  createElement,
+  render,
+};
+
+export default Hypereact;
