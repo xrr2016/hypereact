13c4785498e61dbf6df030d5b8fceddcd1d23ad3
xrr2016 <xiaoranran1993@outlook.com>
2023-10-20 15:42:47 +0800

    feat: update 

--- packages/example/App.tsx
+++ packages/example/App.tsx
@@ -15,15 +15,8 @@ const App = (
     >
       <img src={javascriptLogo} class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Hypereact!</h1>
+    <h1>Hypereact</h1>
     <p class="read-the-docs">Click on the Vite logo to learn more</p>
-    <ul>
-      <ol>任务1</ol>
-      <ol>任务2</ol>
-      <ol>任务3</ol>
-      <ol>任务4</ol>
-      <ol>任务5</ol>
-    </ul>
   </div>
 );
 
